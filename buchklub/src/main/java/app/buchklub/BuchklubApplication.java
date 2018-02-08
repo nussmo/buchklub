8b0495a259498c2fae71919a2f2c7159a532a5ce
Monique Nussbaumer <monique.nussbaumer@students.fhnw.ch>
2018-02-08 22:48:55 +0100

    blank spring boot and react project

---
+++ buchklub/src/main/java/app/buchklub/BuchklubApplication.java
@@ -0,0 +1,12 @@
+package app.buchklub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuchklubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuchklubApplication.class, args);
+	}
+}
